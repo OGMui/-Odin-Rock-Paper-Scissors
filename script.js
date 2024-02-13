39a1efe100b7c53ee2e26f68d45def4b5b4302cf
OGMui <80379201+OGMui@users.noreply.github.com>
2024-02-13 14:25:37 +0700

    Add index.html and script.js boilerplate

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Return random 'Rock', 'Paper' or 'Scissors'
+
+function getComputerChoice (){
+
+}
+
+// Function to play single round of Rock, Paper Scissors
+// Takes 2 parameters 'playerSelection' and 'computerSelection
+// playerSelection must be case-insensitive
+// playerSelection comes from prompt()
+// returns a string that declares the winner or tie
+
+// Function called 'playGame()' which includes the previous functions
+// Game will be played 5 times, can be in a loop or called 5 times
+
+
